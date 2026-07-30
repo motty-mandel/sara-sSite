779ea5ef95ff7c1bdd85bb20db09f96257b288db
motty-mandel <mandelmottyisrael65@gmail.com>
2026-07-30 14:50:30 +0300

    added weather

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const welcomeBanner = document.getElementById('welcomeBanner');
 const imageEl = document.getElementById('image');
+const weather = document.getElementById('weather');
 
 const petnames = [
     "baby",
@@ -29,3 +30,36 @@ const image = imageSources[imageIndex];
 imageEl.innerHTML = `
     <img src="./${image}" alt="pic-of-us" height="auto" width="100%">
 `
+
+async function getWeather() {
+    try {
+        const response = await fetch("https://api.open-meteo.com/v1/forecast?latitude=31.769&longitude=35.2163&current=temperature_2m,is_day&timezone=auto");
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        console.log(data);
+
+        currentTemp = Math.trunc(data.current.temperature_2m);
+        weather.innerHTML = `
+        <h2>The weather today is <br/> ${currentTemp} degrees celsius!</h2>
+    `
+        if (data.current.is_day === 1) {
+            welcomeBanner.innerHTML = `
+                 <h1>Good morning ${petname}!</h1>
+             `;
+        } else {
+             welcomeBanner.innerHTML = `
+                 <h1>Good night ${petname}!</h1>
+             `;
+        }
+
+
+    } catch (error) {
+        console.error('Fetch operation failed:', error);
+    }
+}
+
+getWeather();
