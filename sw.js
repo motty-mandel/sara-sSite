1ec1f9ebcab72c3406dde01d169994b92f845b61
matt_pats <116768184+motty-mandel@users.noreply.github.com>
2026-08-25 01:12:29 +0300

    Update sw.js
    Trying to fix service workers

--- sw.js
+++ sw.js
@@ -1,7 +1,17 @@
-self.addEventListener('install', () => {
-    self.skipWaiting();
+const CACHE_NAME = 'my-baby-v1';
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
-    event.waitUntil(self.clients.claim());
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .catch(() => caches.match(event.request)
+        .then(response => response || caches.match('./index.html')))
+  );
 });
