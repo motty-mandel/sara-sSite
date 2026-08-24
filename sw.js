29425d67f2401f038719c6bd86cc5d5c4a5becf7
matt_pats <116768184+motty-mandel@users.noreply.github.com>
2026-08-25 01:22:45 +0300

    Update sw.js
    Making the service worker minimalist

--- sw.js
+++ sw.js
@@ -1,17 +1,6 @@
-const CACHE_NAME = 'my-baby-v1';
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
 
-self.addEventListener('install', (event) => {
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', (event) => {
-  event.waitUntil(self.clients.claim());
-});
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    fetch(event.request)
-      .catch(() => caches.match(event.request)
-        .then(response => response || caches.match('./index.html')))
-  );
+self.addEventListener('fetch', event => {
+  event.respondWith(fetch(event.request));
 });
